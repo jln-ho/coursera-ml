4d634d4346eae03d351e149150840f9f4cceac5c
Julian Howes <howes@3s-ing.de>
2015-11-17 11:38:25 +0100

    added ex6

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,19 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
-
+J = 1 / (2*m) * sum(((X * theta) - y) .^ 2);
+theta_ = [0; theta(2:length(theta));];
+J = J + lambda / (2*m) * sum(theta_ .^ 2);
+for (i = 1:length(grad))
+  grad(i) = 1 / m * sum(((X * theta) - y) .* X(:,i)) + ((lambda/m) * theta_(i));
+end
 % =========================================================================
-
-grad = grad(:);
 
 end
--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -8,6 +8,7 @@ function [lambda_vec, error_train, error_val] = ...
 %       for different values of lambda. You are given the training set (X,
 %       y) and validation set (Xval, yval).
 %
+m = length(y);
 
 % Selected values of lambda (you should not change this)
 lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
@@ -39,14 +40,12 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
-
-
-
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	theta = trainLinearReg(X, y, lambda);
+	error_train(i) = 1 / (2*m) * sum(((X * theta) - y) .^ 2);
+	error_val(i) = 1 / (2*length(yval)) * sum(((Xval * theta) - yval) .^ 2);
+end
 
 % =========================================================================
 
